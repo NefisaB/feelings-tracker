f2ed6da972d729ec197ce59d445d9d9e16d2d47c
Nefisa <nefisa.hc@gmail.com>
2025-04-15 11:27:02 +0200

    Initial commit

---
+++ src/main/java/com/nefisa/feelingstracker/FeelingsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.nefisa.feelingstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelingsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelingsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nefisa/feelingstracker/FeelingstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nefisa.feelingstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelingstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
