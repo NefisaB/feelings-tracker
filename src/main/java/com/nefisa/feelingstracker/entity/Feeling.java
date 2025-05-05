31c9a7a6c47f4f8be8f7e1bfda52f20634206d3b
Nefisa <nefisa.hc@gmail.com>
2025-05-05 10:14:25 +0200

    Add User, Authority entities and update Feeling entity

--- src/main/java/com/nefisa/feelingstracker/entity/Feeling.java
+++ src/main/java/com/nefisa/feelingstracker/entity/Feeling.java
@@ -6,12 +6,17 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
+@Table(name = "feelings")
 @Entity
 public class Feeling {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
 
     @Column(nullable = false)
     private String description;
@@ -20,24 +25,38 @@ public class Feeling {
     @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime dateAdded;
 
+    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm")
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm")
+    private LocalDateTime dateUpdated;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
     public Feeling() {
     }
 
-    public Feeling(String description) {
-        this.description = description;
-    }
-
-    public Feeling(String description, LocalDateTime dateAdded) {
+    public Feeling(String title, String description, LocalDateTime dateAdded, User owner) {
+        this.title = title;
         this.description = description;
         this.dateAdded = dateAdded;
+        this.owner = owner;
     }
 
-    public Long getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
@@ -54,5 +73,21 @@ public class Feeling {
 
     public void setDateAdded(LocalDateTime dateAdded) {
         this.dateAdded = dateAdded;
+    }
+
+    public LocalDateTime getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(LocalDateTime dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
     }
 }
---
+++ src/main/java/com/nefisa/feelingstracker/entity/User.java
@@ -0,0 +1,106 @@
+package com.nefisa.feelingstracker.entity;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Table(name = "users")
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_authorities",
+    joinColumns = @JoinColumn(name = "user_id"))
+    private List<Authority> authorities;
+
+    @OneToMany(mappedBy = "owner",
+    cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Feeling> feelings;
+
+    public User() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public List<Feeling> getFeelings() {
+        return feelings;
+    }
+
+    public void setFeelings(List<Feeling> feelings) {
+        this.feelings = feelings;
+    }
+}
